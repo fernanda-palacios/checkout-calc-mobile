0bdff4de268307e1c86328085a0b93c9dfccc101
shiseru <nathaniel.naka@gmail.com>
2020-10-01 01:22:33 +0900

    integrated web app.js with mobile app.js

--- App.js
+++ App.js
@@ -4,9 +4,21 @@ import { StyleSheet, Text, View, YellowBox } from 'react-native';
 import firebase from './firebase';
 
 export default function App() {
+  const [itemName, setItemName] = useState('');
+  const [pricePerUnit, setPricePerUnit] = useState('');
+  const [quantity, setQuantity] = useState('');
+
+  // values that user is editing but hasn't yet applied
+  const [newTaxPercentage, setNewTaxPercentage] = useState('');
+  const [newDiscountPercentage, setNewDiscountPercentage] = useState('');
+
+  // actual applied/saved values (these would come from the back-end)
+  const [savedTaxPercentage, setSavedTaxPercentage] = useState(0);
+  const [savedDiscountPercentage, setSavedDiscountPercentage] = useState(0);
+
   const [allItems, setAllItems] = useState([]);
-  const [discountPercentage, setDiscountPercentage]  = useState(0);
-  const [taxPercentage, setTaxPercentage] = useState(0);
+  const [subtotal, setSubtotal] = useState(0);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,19 +30,46 @@ export default function App() {
             });
             const dataId = 1;
             setAllItems(data[dataId].items);
-            setDiscountPercentage(data[dataId].discount_percentage);
-            setTaxPercentage(data[dataId].tax_percentage);
+            setSavedDiscountPercentage(data[dataId].discount_percentage);
+            setSavedTaxPercentage(data[dataId].tax_percentage);
           }
       );
     };
     fetchData();
+    calculateTotal();
   }, []);
+
+  // If data already exists, it updates the values on data. Otherwise, it creates the new data
+  const updateFirestoreData = (items, discountPercentage, taxPercentage) => {
+    firebase.firestore().collection("checkoutCalculator").doc('1').set({
+      items: items,
+      discount_percentage: Number(discountPercentage),
+      tax_percentage: Number(taxPercentage),
+    }).then(function () {
+      console.log("data updated successfully");
+    })
+  };
+
+  const calculateTotal = () => {
+    let subtotal = 0;
+    allItems.forEach((item) => {
+      const itemTotal = item.pricePerUnit * item.quantity;
+      subtotal += itemTotal;
+    });
+
+    const tax = subtotal * (savedTaxPercentage / 100);
+    const discount = subtotal * (savedDiscountPercentage / 100);
+    const total = subtotal + tax - discount;
+
+    setSubtotal(subtotal);
+    setTotal(total);
+  };
 
   return (
     <View style={styles.container}>
       <Text>item Name : {Object.values(allItems).map(data => data['itemName'])}</Text>
-      <Text>the tax percentage is {taxPercentage}</Text>
-      <Text>the discount percentage is {discountPercentage}</Text>
+      <Text>the tax percentage is {savedDiscountPercentage}</Text>
+      <Text>the discount percentage is {savedTaxPercentage}</Text>
       <StatusBar style="auto" />
     </View>
   );
