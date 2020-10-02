b560e684884902d6dd384eb67945ec29b66fdcfe
FernandaPalacios <ferpalacios.a@gmail.com>
2020-10-02 13:57:42 -0500

    initialize firebase exactly as described in docs

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import * as firebase from 'firebase';
-import firestore from 'firebase/firestore'
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB6iGj2vKINoTHilVelxZSksX3lzhL51qM",
